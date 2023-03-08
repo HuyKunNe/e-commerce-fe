0368634a1bf5c2abec48489b703bc5bef1253951
DQHUY <dinh.quang.huy@quantic.com.vn>
2023-03-08 16:46:20 +0700

    change favicon + title

--- src/router.js
+++ src/router.js
@@ -8,25 +8,31 @@ const routes = [
         name: "Home",
         component: HomePage,
         path: "/",
+        meta: {
+            title: "BAD HABITS | BADDEST SINCE 2017 – Bad Habits Official Store",
+        },
     },
     {
         name: "Account",
         path: "/account",
         children: [
-            {   
+            {
                 name: "Login",
                 path: "login",
                 component: LoginPage,
+                meta: { title: "Tài khoản – Bad Habits Official Store" },
             },
-            {   
+            {
                 name: "Register",
                 path: "register",
                 component: RegisterPage,
+                meta: { title: "Tạo tài khoản – Bad Habits Official Store" },
             },
-            {   
+            {
                 name: "Profile",
                 path: "profile",
                 component: ProfilePage,
+                meta: { title: "Tài khoản – Bad Habits Official Store" },
             },
         ],
     },
@@ -35,6 +41,12 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+router.beforeEach((to) => {
+    document.title =
+        to.meta.title ||
+        "BAD HABITS | BADDEST SINCE 2017 – Bad Habits Official Store";
+    console.log(to.meta.title);
 });
 
 export default router;
